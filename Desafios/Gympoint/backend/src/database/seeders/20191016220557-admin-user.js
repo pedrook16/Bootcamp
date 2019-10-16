4649beea470e1d17d1df737c34f90b41dfbe6bed
Pedro Henrique <pedrook16@gmail.com>
2019-10-16 19:22:31 -0300

    add

---
+++ Desafios/Gympoint/backend/src/database/seeders/20191016220557-admin-user.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Administrador',
+          email: 'admin@gympoint.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: queryInterface => {
+    return queryInterface.bulkDelete('users', null, {});
+  },
+};
