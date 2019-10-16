a3668cd3209e17fa6f2348be9192464ad9034c1b
Pedro Henrique <pedrook16@gmail.com>
2019-10-16 20:41:54 -0300

    admin authentication

--- Desafios/Gympoint/backend/src/app/models/User.js
+++ Desafios/Gympoint/backend/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs'
 
 class User extends Model {
   static init(sequelize) {
@@ -6,12 +7,17 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
--- Desafios/Gympoint/backend/src/routes.js
+++ Desafios/Gympoint/backend/src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController'
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  res.json({ ok: true });
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
