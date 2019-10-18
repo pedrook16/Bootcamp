c59a27261b765e769cc885eb3f9c7a6266e1f91b
Pedro Henrique <pedrook16@gmail.com>
2019-10-18 16:55:45 -0300

    add

--- Node/goBarber/src/app/schemas/Notification.js
+++ Node/goBarber/src/app/schemas/Notification.js
@@ -1,21 +1,24 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from 'mongoose';
 
-const NotificationSchema = new Schema({
-  content: {
-    type: String,
-    required: true
+const NotificationSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  user: {
-    type: Number,
-    required: true
-  },
-  read: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
- }, {
-  timestamps: true
-})
+  {
+    timestamps: true,
+  }
+);
 
-export default model('Notification', NotificationSchema)
+export default model('Notification', NotificationSchema);
---
+++ Node/goBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c5a7fc55a76d13',
+    pass: '6b451a1a6e4f5f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
