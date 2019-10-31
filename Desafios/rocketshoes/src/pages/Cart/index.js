be3059a519af3a8c2c2fa94ebb122d856d9d4449
Paulo Victor <paulovictor@iMac-de-Paulo.local>
2019-10-31 20:01:39 -0300

    init flux

--- Desafios/rocketshoes/src/pages/Cart/index.js
+++ Desafios/rocketshoes/src/pages/Cart/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import tenis from '../../assets/tenis.jpg'
+
+import { 
+  Container,
+  Products,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+  Icon } from './styles';
 
 export default function Cart() {
   return (
-    <View />
+    <Container>
+      <Products>
+        <Product>
+          <ProductInfo>
+            <ProductImage source={tenis} />
+            <ProductDetails>
+              <ProductTitle>tenis legal</ProductTitle>
+              <ProductPrice>R$100,00</ProductPrice>
+            </ProductDetails>
+            <ProductDelete >
+              <Icon>del</Icon>
+            </ProductDelete>
+          </ProductInfo>
+          <ProductControls>
+            <ProductControlButton >
+              <Icon>-</Icon>
+            </ProductControlButton>
+            <ProductAmount value="3" />
+            <ProductControlButton >
+              <Icon>+</Icon>
+            </ProductControlButton>
+            <ProductSubtotal>R$100,00</ProductSubtotal>
+          </ProductControls>
+        </Product>         
+        </Products>
+        <TotalContainer>
+          <TotalText>TOTAL</TotalText>
+          <TotalAmount>R$100,00</TotalAmount>
+          <Order>
+            <OrderText>FINALIZAR PEDIDO</OrderText>
+          </Order>
+        </TotalContainer>
+    </Container>
   );
 }
