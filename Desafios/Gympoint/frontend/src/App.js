49c45253738e273a65d88fa0ca9916098f68c1fd
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-05 19:46:35 -0300

    gympoint frontend

---
+++ Desafios/Gympoint/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Router from './routes';
+import GlobalStyled from './styles/Global';
+
+function App() {
+    return (
+        <>
+            <Router />
+            <GlobalStyled />
+        </>
+    );
+}
+
+export default App;
---
+++ Desafios/Gympoint/frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Sign from './pages/Sign';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/sign" component={Sign} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
