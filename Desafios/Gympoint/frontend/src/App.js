c3d47a2f310cf7070c4de8d76c3729512eb53f59
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-07 16:40:41 -0300

    reactotron

--- Desafios/Gympoint/frontend/src/App.js
+++ Desafios/Gympoint/frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import GlobalStyled from './styles/Global';
-import Routes from './routes';
 
+import '~/config/ReactotronConfig';
+
+import Routes from '~/routes';
 import history from '~/services/history';
 
 function App() {
