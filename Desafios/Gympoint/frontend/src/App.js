8482888d9cfd6d8b825e04bc07bff5d2f821b58b
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-07 16:36:39 -0300

    routes

--- Desafios/Gympoint/frontend/src/App.js
+++ Desafios/Gympoint/frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Router from './routes';
+import { Router } from 'react-router-dom';
 import GlobalStyled from './styles/Global';
+import Routes from './routes';
+
+import history from '~/services/history';
 
 function App() {
     return (
-        <>
-            <Router />
+        <Router history={history}>
+            <Routes />
             <GlobalStyled />
-        </>
+        </Router>
     );
 }
 
